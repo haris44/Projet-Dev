8c2504964e75dae2ccddc554b2c84d089d6bca02
Alexandre BERTRAND <lxndr.bertrand@gmail.com>
2015-11-24 13:36:00 +0100

    Démmarage du projet

---
+++ Air Atlantique/Air Atlantique/Template.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Air_Atlantique
+{
+    /// <summary>
+    /// Logique d'interaction pour Template.xaml
+    /// </summary>
+    public partial class Template : Page
+    {
+        public Template()
+        {
+            InitializeComponent();
+           
+        }
+    
+    public void mailNotif(int nombre)
+        {
+            if (nombre <= 0)
+                notifCircle.Visibility = Visibility.Collapsed;
+            else
+            {
+                notifCircle.Visibility = Visibility.Visible;
+                notif.Content = nombre.ToString();
+            }
+        }
+    }
+}
